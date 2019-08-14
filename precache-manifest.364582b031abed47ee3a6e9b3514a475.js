0320aec7029f460d51927bd003dbc717d472b907
Alberto Zaranza <albertozaranza@outlook.com>
2019-08-14 04:06:10 -0300

    Updates

---
+++ precache-manifest.364582b031abed47ee3a6e9b3514a475.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ae16e5be00fc5c7a572c14f37ea7c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f14c66a7c5483a407c47",
+    "url": "/static/css/main.48aaf718.chunk.css"
+  },
+  {
+    "revision": "15f5d9d45d1befdb26b4",
+    "url": "/static/js/2.611a48d7.chunk.js"
+  },
+  {
+    "revision": "f14c66a7c5483a407c47",
+    "url": "/static/js/main.1347cd6f.chunk.js"
+  },
+  {
+    "revision": "7b73e128dec9e382745d",
+    "url": "/static/js/runtime~main.953fe262.js"
+  },
+  {
+    "revision": "b01e3c4e22ae6ce883e1bff29e5f8f1b",
+    "url": "/static/media/background.b01e3c4e.gif"
+  },
+  {
+    "revision": "5703fd3845e704117c0b619eea9831fc",
+    "url": "/static/media/logo-g1.5703fd38.svg"
+  },
+  {
+    "revision": "472a992bd466b03bbea181384ff99a52",
+    "url": "/static/media/logo-globo.472a992b.svg"
+  }
+]);
